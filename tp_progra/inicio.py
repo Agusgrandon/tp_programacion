7505235af7540232428567a22921ea4359f342f6
Maiten Lopez Hidalgo <maitenlopezhidalgook@gmail.com>
2026-05-01 19:16:16 -0300

    agregamos datos pedidos

--- tp_progra/inicio.py
+++ tp_progra/inicio.py
@@ -1,2 +1,19 @@
-print("hola")
-print("hola 2")
+cpu = int(input("Ingrese % de uso de CPU: "))
+ram = int(input("Ingrese % de memoria RAM: "))
+espacio_libre_gb = int(input("Espacio libre en disco: "))
+us_conectados = int(input("Us. conectados"))
+procesos_activos = int(input("Ingrese cantidad de procesos activos: "))
+
+sistema_operativo = input("Linux / Windows Server")
+estado_firewall = input("Activo / Inactivo")
+servidor = input("(Web / Base de datos / Archivos")
+
+nombre_servidor = input("Nombre del servidor: ")
+nombre_administrador = input("Nombre del administrador responsable: ")
+
+#8 condiciones minimos
+
+#regla 1: carga total
+carga_total = (cpu + ram) / 2
+
+#regla 2: nivel estimado de riesgo
