57af38888fb452a8375d5208afb7b844b1de706a
Camila Eva <camilaevavelasquez1@gmail.com>
2026-05-04 19:08:25 -0300

    correcciones en los input

--- tp_progra/inicio.py
+++ tp_progra/inicio.py
@@ -1,4 +1,4 @@
-def procesar_datos(cpu, ram, espacio_libre_gb, us_conectados, procesos_activos, estado_firewall):
+def procesar_datos(cpu, ram, espacio_libre_gb, us_conectados, procesos_activos, estado_firewall,servidor):
   
     #calculo 1: carga total
     carga_total = (cpu + ram) / 2
@@ -8,16 +8,16 @@ def procesar_datos(cpu, ram, espacio_libre_gb, us_conectados, procesos_activos, 
     recursos_disponibles = espacio_libre_gb - (procesos_activos * 0.3)
     
     #regla 1
-    if not estado_firewall == "Inactivo": 
+    if not estado_firewall == "inactivo": 
         estado_de_la_computadora = "Computadora segura"
     #regla 2
-    elif cpu > 85 and ram > 80 and estado_firewall == "Inactivo":
+    elif cpu > 85 and ram > 80 and estado_firewall == "inactivo":
         estado_de_la_computadora = "Estado critico"
     #regla 3
     elif us_conectados > 1000 or procesos_activos > 1000:
         estado_de_la_computadora = "Sistema saturado"
     #regla 4
-    elif servidor == "Web" and us_conectados > 100 and cpu > 75:
+    elif servidor == "web" and us_conectados > 100 and cpu > 75:
         estado_de_la_computadora = "Alta demanda"
     #regla 5
     elif espacio_libre_gb < 10 or procesos_activos:
@@ -47,11 +47,11 @@ ram = int(input("Ingrese % de memoria RAM: "))
 while ram < 0:
     ram = int(input("Error, el numero ingresado no es correcto, reingresalo: "))
 
-espacio_libre_gb = int(input("Espacio libre en disco: "))
+espacio_libre_gb = int(input("Ingrese su espacio libre en disco (GB): "))
 while espacio_libre_gb < 0:
     espacio_libre_gb = int(input("Error, el numero ingresado no es correcto, reingresalo: "))
 
-us_conectados = int(input("Us. conectados"))
+us_conectados = int(input("Ingrese cantidad de usuarios conectados: "))
 while us_conectados < 0:
     us_conectados = int(input("Error, el numero ingresado no es correcto, reingresalo: "))
 
@@ -59,27 +59,24 @@ procesos_activos = int(input("Ingrese cantidad de procesos activos: "))
 while procesos_activos < 0:
     procesos_activos = int(input("Error, el numero ingresado no es correcto, reingresalo: "))
 
-sistema_operativo = input("Linux / Windows Server")
+sistema_operativo = input("Ingrese sistema operativo (linux / windows server): ")
 while sistema_operativo != "linux" and sistema_operativo != "windows server":
     sistema_operativo = input("Error, reingresa el sistema operativo (linux / windows server): ")
 
-estado_firewall = input("Activo / Inactivo")
+estado_firewall = input("Ingrese estado del firewall (activo / inactivo): ")
 while estado_firewall != "activo" and estado_firewall != "inactivo":
     estado_firewall = input("Error, informa nuevamente el estado del firewall (activo / inactivo): ")
 
-servidor = input("(Web / Base de datos / Archivos")
+servidor = input("(Ingrese tipo de servidor (web / base de datos / archivos): ")
 while servidor != "web" and servidor != "base de datos" and servidor != "archivos":
     servidor = input("Error, informa nuevamente el tipo de servidor (web / base de datos / archivos): ")
 
-nombre_servidor = input("Nombre del servidor: ")
+nombre_servidor = input("Ingrese el nombre del servidor: ")
 while nombre_servidor == "":
     nombre_servidor = input("Error, ingresa nuevamente el nombre del servidor: ")
 
-nombre_administrador = input("Nombre del administrador responsable: ")
+nombre_administrador = input("Ingrese el nombre del administrador responsable: ")
 while nombre_administrador == "":
     nombre_administrador = input("Error, ingresa nuevamente el nombre del administrador: ")
 
 variable_a = procesar_datos ()
-
-
-
